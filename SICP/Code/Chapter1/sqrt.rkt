c867e1b452d30071dc0ebe7b34e055fba135580e
Fontaine_PC_Win10 <fontaine_gzy@163.com>
2018-08-30 16:53:54 +0800

    sqrt

---
+++ SICP/Code/Chapter1/sqrt.rkt
@@ -0,0 +1,25 @@
+#lang planet neil/sicp
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
+
+(define (abs x)
+  (cond ((< x 0)(- x))
+        ((= x 0) 0)
+        ((> x 0) x)))
+
+
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+
+(sqrt 100)
