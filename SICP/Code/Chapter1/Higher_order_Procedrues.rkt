9cb2fdcb757ab339fa1565e61aea88d41413bf11
Fontaine_PC_Win10 <fontaine_gzy@163.com>
2018-09-02 17:10:45 +0800

    High_order_procedure

---
+++ SICP/Code/Chapter1/Higher_order_Procedrues.rkt
@@ -0,0 +1,15 @@
+#lang planet neil/sicp
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+
+(define (cube n) (* (* n n) n))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(sum-cubes 1 10)
